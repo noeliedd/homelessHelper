aeb9f92baccdc5313cb9b18ca97928981dd3f869
noeliedd <noel.devitt@student.ncirl.ie>
2015-05-18 18:24:21 +0000

    open login page everytime app started

--- hhApp/www/js/app.js
+++ hhApp/www/js/app.js
@@ -16,10 +16,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 })
 
 .config(function($stateProvider, $urlRouterProvider,$httpProvider) {
-  if(window.localStorage.currentUser) {
-	$urlRouterProvider.otherwise('/routeSelection');
+  console.log(window.localStorage.currentUser);
+  if(window.localStorage.currentUser) {   
+	    $urlRouterProvider.otherwise('/routeSelection');   
   }else{
-    $urlRouterProvider.otherwise('/login');
+      $urlRouterProvider.otherwise('/login');
   }
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -96,7 +97,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         }
       }
     })
-  $urlRouterProvider.otherwise('/login');
+//   $urlRouterProvider.otherwise('/login');
   
    $httpProvider.interceptors
    .push(function($q, $location)
